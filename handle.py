13a4808984672462f87b7916bed1face684a1542
kok-s0s <2694308562@qq.com>
2023-12-12 04:21:06 +0800

    combine data

--- handle.py
+++ handle.py
@@ -4,27 +4,29 @@ import json
 from bs4 import BeautifulSoup
 
 
-def process_markdown_file(file_path, output_folder="json"):
-    with open(file_path, "r", encoding="utf-8") as file:
-        markdown_content = file.read()
+def process_markdown_files(file_paths, output_folder="json"):
+    all_data = []
 
-        html_content = markdown.markdown(markdown_content)
+    for file_path in file_paths:
+        with open(file_path, "r", encoding="utf-8") as file:
+            markdown_content = file.read()
 
-        soup = BeautifulSoup(html_content, "html.parser")
+            html_content = markdown.markdown(markdown_content)
 
-        h1_title = soup.h1.text
+            soup = BeautifulSoup(html_content, "html.parser")
 
-        li_texts = [li.p.text for li in soup.find_all("li")]
+            h1_title = soup.h1.text
 
-    output_data = {"title": h1_title, "li_texts": li_texts}
+            li_texts = [li.p.text for li in soup.find_all("li")]
 
-    output_file_name = os.path.join(
-        output_folder, f"{os.path.basename(file_path)[:-2]}json"
-    )
+        file_data = {"title": h1_title, "li_texts": li_texts}
+        all_data.append(file_data)
+
+    output_file_name = os.path.join(output_folder, "combined_output.json")
 
     with open(output_file_name, "w", encoding="utf-8") as json_file:
-        json.dump(output_data, json_file, ensure_ascii=False, indent=2)
+        json.dump(all_data, json_file, ensure_ascii=False, indent=2)
 
 
-process_markdown_file("markdown/Algorithm.md")
-process_markdown_file("markdown/Living-Documentation.md")
+file_paths = ["markdown/Algorithm.md", "markdown/Living-Documentation.md"]
+process_markdown_files(file_paths)
